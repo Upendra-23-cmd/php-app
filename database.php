0eccdb2f091246c4165fc52c748d65e41c2fa813
upendra-23-cmd <vrambharat05@icloud.com>
2026-04-22 00:20:44 +0530

    new bugs solved

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_HOST', getenv('DB_HOST') ?: 'db');
 define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASS') ?: 'admin123');
+define('DB_PASS', getenv('DB_PASSWORD') ?: 'admin123');
 define('DB_NAME', getenv('DB_NAME') ?: 'todo_app');
 
 function getConnection() {
